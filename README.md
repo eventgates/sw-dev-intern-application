1ecb4a361ca47b2e0b6217ce311869edc28ee962
kemal Celikel <kemal409@hotmail.com>
2023-09-22 14:24:43 +0300

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+## Event Gates Software Developer Internship Application
+
+* Use python 3.6+
+* Do not change the function names, grader will be using them for testing.
+* This offline application is only a filter, it will be reviewed by the dev team later on for applicant who passed a certain percentage.
+* Create a public github gist and send the link with an email. Attach your cv as a pdf file named as firstname_lastname_cv.pdf 
+* In the email, explain in a paragraph how you could try to limit the usage of ChatGPT in an application such as this one.
+
---
+++ application.py
@@ -0,0 +1,49 @@
+"""
+Event Gates Software Developer Internship Application
+
+* Use python 3.6+
+* Do not change the function names, grader will be using them for testing.
+* This offline application is only a filter, it will be reviewed by the dev team later on for applicant who passed a certain percentage.
+* Create a public github gist and send the link with an email. Attach your cv as a pdf file named as firstname_lastname_cv.pdf 
+* In the email, explain in a paragraph how you could try to limit the usage of ChatGPT in an application such as this one.
+"""
+
+
+def sum_values(data):
+    """
+    Question 1: Basic Data Structures
+    Write a function sum_values(data) that takes a dictionary as an argument and returns the sum of all values that are integers or floats.
+    """
+    pass
+
+
+def even_squares(n):
+    """
+    Question 2: List Comprehensions
+    Write a function even_squares(n) that returns a list of squares of even numbers up to n.
+    """
+    pass
+
+
+def is_balanced(s):
+    """
+    Question 3: Stacks and Queues
+    Write a function is_balanced(s) that checks if a string with brackets is balanced. For simplicity, consider only ( and ).
+    """
+    pass
+
+
+def reverse_words(sentence):
+    """
+    Question 4: String Manipulation
+    Write a function reverse_words(sentence) that reverses the words in a sentence.
+    """
+    pass
+
+
+def find_max(nums):
+    """
+    Question 5: Basic Algorithms
+    Write a function find_max(nums) that returns the maximum number in a list without using the built-in max function.
+    """
+    pass
